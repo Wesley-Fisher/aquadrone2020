5c4828945d608c28c8166940fdb2bd8ded456fb0
Amaar Quadri <amaarquadri@gmail.com>
2019-06-22 19:06:23 -0400

    created ros package

---
+++ catkin_ws/src/motor_control/src/Main_Keyboard.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import numpy as np
+import rospy
+from geometry_msgs.msg import Wrench
+
+#wait for motors to set up
+rospy.sleep(7)
+
+rospy.init_node("main")
+publisher = rospy.Publisher("movement_command", Wrench, queue_size=0)
+
+while True:
+	x = 0
+	z = 0
+	yaw = 0
+	
+	control = input("Command:")
+	if control == "w":
+		x = 1
+	elif control == "s":
+		x = -1
+	elif control == "a":
+		yaw = 1
+	elif control == "d":
+		yaw = -1
+	elif control == "i":
+		z = 1
+	elif contorl == "k":
+		z = -1
+	
+	command = Wrench()
+	command.force.x = x
+	command.force.z = z
+	command.torque.z = yaw 
+	publisher.publish(command)
